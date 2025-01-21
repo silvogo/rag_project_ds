49db46e02b99385746f3745a53e657c3a172ec58
Diogo Silva <sdiogo27@gmail.com>
2025-01-21 00:12:06 +0000

    API added. Chunking and data processing refined

--- src/chain.py
+++ src/chain.py
@@ -1,28 +1,55 @@
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+)
 from langchain_core.output_parsers import StrOutputParser
 
+from src.retriever import create_vector_store_indexed
+
 # Load environment variables
-load_dotenv()
+# load_dotenv()
 
 
-def create_chain(retriever):
+embedding_function = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en")
+vector_store = create_vector_store_indexed(embedding_function)
+retriever = vector_store.as_retriever(search_kwargs={"k": 2})
 
-    system_prompt = """"
+
+# Define the prompt for question answering
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
             You are an expert analyzing customer satisfaction data.
-
             Use the given context to answer the question.
+            <context>
+            {context}
+            </context>  
+            If you don't know the answer, say you don't know.
+            """,
+        ),
+        ("human", "{input}"),
+    ]
+)
 
-            "Context:\n{context}\n\n"
-            "Answer as clearly and concisely as possible. If you don't know the answer say you don't know."""
 
-    prompt = PromptTemplate(input_variables=['context', 'input'], template=system_prompt)
+def get_chain(model="gpt-4o-mini"):
 
-    llm = ChatOpenAI(temperature=0, model_name='gpt-4o-mini')
-    question_answer_chain = create_stuff_documents_chain(llm, prompt, output_parser=StrOutputParser())
-    chain = create_retrieval_chain(retriever=retriever, combine_docs_chain=question_answer_chain)
+    # define the llm
+    llm = ChatOpenAI(temperature=0, model_name=model)
+    question_answer_chain = create_stuff_documents_chain(
+        llm=llm, prompt=qa_prompt, output_parser=StrOutputParser()
+    )
+
+    chain = create_retrieval_chain(
+        retriever=retriever, combine_docs_chain=question_answer_chain
+    )
 
     return chain
--- src/pydantic_models.py
+++ src/pydantic_models.py
@@ -1,12 +1,16 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 
-class ModelName:
-    GPT4_O_MINI = 'gpt-4o-mini'
+
+class ModelName(str, Enum):
+    GPT4_O_MINI = "gpt-4o-mini"
+
 
 class QueryInput(BaseModel):
     question: str
-    session_id: str= Field(default=None)
+    session_id: str = Field(default=None)
     model: ModelName = Field(default=ModelName.GPT4_O_MINI)
+
 
 class QueryResponse(BaseModel):
     answer: str
