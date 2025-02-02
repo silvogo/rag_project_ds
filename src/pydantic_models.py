37f1f05e82136db337e15171c89449e5918f91c7
Diogo Silva <sdiogo27@gmail.com>
2025-02-02 12:24:13 +0000

    Refined API (#8)

--- src/pydantic_models.py
+++ src/pydantic_models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from enum import Enum
 from typing import Optional
 
@@ -9,11 +10,27 @@ class ModelName(str, Enum):
 
 class QueryInput(BaseModel):
     question: str
-    session_id: Optional[str] = None
+    session_id: Optional[str] = Field(default=None,
+                                      description="Optional session ID")
     model: ModelName = Field(default=ModelName.GPT4_O_MINI)
+
+    # defines what json structure of the model should apear in Swagger's example request
+    class Config:
+        json_schema_extra = {
+            "example":{
+                "question": "What is the nps score?",
+                "session_id": None,
+                "model": "gpt-4o-mini"
+            }
+        }
 
 
 class QueryResponse(BaseModel):
     answer: str
     session_id: str
     model: ModelName
+
+class DocumentInfo(BaseModel):
+    id: int
+    filename: str
+    upload_timestamp: datetime
