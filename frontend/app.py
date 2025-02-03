ed818a54634247d7bf74fa6137c3636a59d4c7ef
Diogo Silva <sdiogo27@gmail.com>
2025-02-03 23:26:11 +0000

    Add frontend to RAG Application (#9)

---
+++ frontend/app.py
@@ -0,0 +1,22 @@
+import sys
+import os
+# Add the project root (one level up) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import streamlit as st
+
+from frontend.chat_ui import display_chat
+
+# Definining the title of the APP
+st.title("RAG Customer Satisfaction Chatbot")
+
+# initialize session state variables to not lose previous values
+if "messages" not in st.session_state:
+    # messages is a key in special dictionary (st.session_state) that can be accessed using st.session_state.messages
+    st.session_state.messages = []
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = None
+
+
+# Display chat
+display_chat()
