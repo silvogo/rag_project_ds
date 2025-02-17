7413e6b747657f7d1e3a52a620b3fcac6d88a0fa
Diogo Silva <sdiogo27@gmail.com>
2025-02-17 18:30:03 +0000

    Added sqllite database

---
+++ backend/src/database_utils.py
@@ -0,0 +1,88 @@
+import sqlite3
+
+DBNAME = 'rag_ds_app.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DBNAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_document_table():
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS document_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        document_name TEXT,
+        upload_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+        '''
+    )
+
+    conn.close()
+
+    return True
+
+def create_app_logs():
+    conn = get_db_connection()
+
+    conn.execute(''' 
+        CREATE TABLE IF NOT EXISTS app_logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        session_id TEXT,
+        user_query TEXT,
+        model_response TEXT,
+        model TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+    ''')
+
+    conn.close()
+
+
+def insert_application_logs(session_id, user_query, model_response, model):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        'INSERT INTO app_logs (session_id, user_query, model_response, model) VALUES (?, ?, ?, ? )',
+        (session_id, user_query, model_response, model)
+    )
+    conn.commit()
+    conn.close()
+
+
+def insert_document_record(document_name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO document_data (document_name) VALUES (?)', (document_name))
+    # captures the id of the last inserted document
+    document_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+
+    return document_id
+
+def delete_document_record(file_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM document_store WHERE id = ?', (file_id,))
+    conn.commit()
+    conn.close()
+    return True
+
+
+
+def get_all_documents():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, filename, upload_timestamp FROM document_data ORDER BY upload_timestamp desc')
+
+# init database
+create_app_logs()
+create_document_table()
+
+
+
+
+
